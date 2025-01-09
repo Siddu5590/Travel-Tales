fbd002f198e2bb36a68a68a6795487a4a0b0cc57
Siddu <sidduswamy925@gmail.com>
2025-01-09 23:03:25 +0530

    connection established..

--- Travel Tales/src/main/java/com/travel/Controller/Signup.java
+++ Travel Tales/src/main/java/com/travel/Controller/Signup.java
@@ -1,15 +1,41 @@
 package com.travel.Controller;
 
+import com.travel.Model.Customer;
+
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
-@WebServlet("/register")
+@WebServlet("/signup")
 public class Signup extends HttpServlet {
 
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-	{
-		response.getContentType();
+	protected void processRequest(HttpServletRequest req, HttpServletResponse response) throws Exception {
+
+		response.setContentType("text/html;charset=ISO-8859-1");
+		
+		HttpSession sesssion=req.getSession();
+		Customer c=new Customer(sesssion);
+		
+		try {
+			if(req.getParameter("btn")!=null) {
+				String name=req.getParameter("Fname");
+				String phone=req.getParameter("phno");
+				String mail=req.getParameter("mail");
+				String password=req.getParameter("password");
+				String confirm=req.getParameter("confirm");
+				
+				if(password.equals(confirm)) {
+					String status=c.signup(name,phone,mail,password);
+				}
+				
+				
+			}
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
